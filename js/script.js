4810be2dc4df47cb40c4366bc7a99b02cf98dfa4
mrezafiqri <mrezafqrnnr@gmail.com>
2023-11-14 01:42:31 +0800

    menambahkan produk & modal box produk

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // Toggle class Active untuk Hamburger
 const navbarNav = document.querySelector('.navbar-nav');
 //ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
   navbarNav.classList.toggle('active')
+  e.preventDefault()
 }
 
 // Toggle class Active untuk Search Form
@@ -15,9 +16,18 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault()
 }
 
+// Toggle class Active untuk Shoping Cart 
+const shopingCart = document.querySelector('.shoping-cart')
+
+document.querySelector('#shoping-cart-button').onclick = (e) => {
+  shopingCart.classList.toggle('active');
+  e.preventDefault()
+}
+
 // klik di luar element
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shoping-cart-button');
 
 document.addEventListener('click', (e) => {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -26,4 +36,33 @@ document.addEventListener('click', (e) => {
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove('active')
   }
+  if (!sc.contains(e.target) && !shopingCart.contains(e.target)) {
+    shopingCart.classList.remove('active')
+  }
 })
+
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex'
+    e.preventDefault();
+  }
+})
+
+
+// klik tombol Close 
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+}
+
+// klik di luar modal 
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+}
